95004051ed0fc00663fef7e9f6d28748a4bceece
mar <mar.dls@tutamail.com>
2020-05-24 03:48:55 -0500

    copy input to output. v1

---
+++ 01_tutorial/readchar.c
@@ -0,0 +1,14 @@
+// readchar.c
+#include <stdio.h>
+
+/* copy input to output; 1st version */
+int main() {
+    int c;
+
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+    return 0;
+}
