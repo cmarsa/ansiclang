42165db9cde692d10777e05e0cd77a8838d5df71
mar <mar.dls@tutamail.com>
2020-06-07 12:49:41 -0500

    Reverse polish calculator with different program structure.

---
+++ 04_functions/rev_polish_calc_v2/getch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "calc.h"
+
+#define NUMBER '0'  // signal that a number was found
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp]: getchar();
+}
+
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
