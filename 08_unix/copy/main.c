22702207680c78868b42feb38174452638863d73
mar <mar.dls@tutamail.com>
2020-07-06 23:31:43 -0500

    copy program: copies anything to anything.

---
+++ 08_unix/copy/main.c
@@ -0,0 +1,12 @@
+// main.c
+#include "syscall.h"
+#define BUFSIZE 1024
+
+int main() {
+    char buf[BUFSIZE];
+    int n;
+
+    while ((n = read(0, buf, BUFSIZE)) > 0)
+        write(1, buf, n);
+    return 0;
+}
