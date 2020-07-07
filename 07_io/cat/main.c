0e6733d3406a52848b2066a153113f7d81136d1b
mar <mar.dls@tutamail.com>
2020-07-06 23:15:00 -0500

    Adding stderr redirection.

--- 07_io/cat/main.c
+++ 07_io/cat/main.c
@@ -1,5 +1,6 @@
 // main.c
 #include <stdio.h>
+#include <stdlib.h>
 #define getchar()   getc(stdin)
 #define putchat()   putc((c), stdout)
 
@@ -10,19 +11,24 @@
 int main(int argc, char *argv[]) {
     FILE *fp;
     void filecopy(FILE *, FILE *);
+    char *prog = argv[0];
 
-    if (argc == 1)
+    if (argc == 1)      // no args; copy standard input
         filecopy(stdin, stdout);
     else
         while (--argc > 0)
             if ((fp = fopen(*++argv, "r")) == NULL) {
-                printf("cat: can\'t open %s\n", *argv);
-                return 1;
+                fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+                exit(1);
             } else {
                 filecopy(fp, stdout);
                 fclose(fp);
             }
-        return 0;
+    if (ferror(stdout)) {
+        fprintf(stderr, "%s: error writing stdout\n", prog);
+        exit(2);
+    }
+    exit(0);
 }
 
 
@@ -35,3 +41,8 @@ void filecopy(FILE *ifp, FILE *ofp) {
         putc(c, ofp);
 }
 
+
+/**
+ * Within main , return expr is equivalent to exit ( expr ). exit has the advantage that it can be
+ * called from other functions, and that calls to it can be found with a pattern-searching program
+ */
